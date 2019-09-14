4b6986e9e3c7edb57e2a4628cddaa945d6675125
Ngoc Tu <buingoctu1998@gmail.com>
2019-09-14 15:22:06 +0700

    read file

--- Exercise1.c
+++ Exercise1.c
@@ -12,22 +12,23 @@ typedef struct {
   char room[50];
 }COURSE;
 
+void readFile(char* filename);
+
 int main(int argc, char *argv[]){
-  COURSE course; // malloc cho cac phan tu
+  COURSE course[10]; // malloc cho cac phan tu
   if (argc>1)
   {
-    readFile(argv[1]);
-  }
+    char* filename =  argv[1];
+    readFile(filename);
+    
+  }else printf("No source file found!");
   
-  
-
-
-
   return 0;
 }
 
-int readFile(char* filename){ // tra ve chuoi~ cai string de xu ly
+void readFile(char* filename){ 
   // char *list = (char*)malloc(sizeof(char)*200);
+  COURSE course[10];
   char list[10][1000];
   FILE *fp;
   int c;
@@ -35,23 +36,18 @@ int readFile(char* filename){ // tra ve chuoi~ cai string de xu ly
   fp = fopen(filename,"r");
   if(fp == NULL){
     printf("Error while opening file.");
-    return 1;
   }
   while( (c=fgetc(fp)) != EOF ){   
     if(c != ';'){
       char buff= (char) c;
-      // printf("%c",buff);      
-      strcat(list[courseNum],(char[]){buff, '\0'});    
-      
-      
+      strcat(list[courseNum],(char[]){buff, '\0'});      
     }else{
       courseNum++;
       printf("");
-    }
-    
+    }    
   }
   fclose(fp); 
   for(int i=0;i<courseNum;i++)
   printf("\n\n%s",list[i]);
-  return 0;
+  // return course[0];
 }
